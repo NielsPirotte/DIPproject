a8dae2f7b9793b5b654fb75e288c651fcadcd72e
Niels Pirotte <niels.pirotte@student.uhasselt.be>
2017-10-23 18:00:44 +0200

    Expirimentations with operations

--- project1.m
+++ project1.m
@@ -1,9 +1,11 @@
+%% Section 1 Background frame
+
 close all;
 clear;
 clc;
 
 % create video  object
-vid = VideoReader('wandeling_1a.mp4');
+vid = VideoReader('Wandeling_1a.mp4');
 
 %Get properties from video
 framerate = vid.framerate;
@@ -16,37 +18,68 @@ vidWidth = vid.Width;
 
 %parameters
 lengthWalk = 3.15; %lengte van de wandeling
+medf = [3 3];
 
 %%Achtergrond
 achtergrondFrame = read(vid, 1);
-figure, imshow(achtergrondFrame)
+%figure, imshow(achtergrondFrame)
 xAT = rgb2gray(achtergrondFrame);
 xA = im2double(xAT);
+%figure('name', 'gray'), imshow(xA)
+se_tekst = strel('rectangle', [5 5]);
+xA = imclose(xA, se_tekst);
+%figure('name', 'filter_tekst'), imshow(xA)
+%imopen
+xA = medfilt2(xA, medf);
 
-for i=135:160
-%%Voorgrond
-voorgrondFrame = read(vid, i);
-%%figure, imshow(voorgrondFrame)
-xVT = rgb2gray(voorgrondFrame);
-xV = im2double(xVT);
-
-%%Verschil
-xVerschil = xA - xV;
-fBlack = xVerschil>0.2;
-%%figure, imshow(xVerschil)
-%%figure, imhist(xVerschil)
-%%figure, imshow(fBlack)
-%%Opening/closing
-se = strel('disk', 20);
-fo = imopen(fBlack,se);
-imshow(fo)
-end
-
-
-for i=1:no_frames
+%% section 2 Forground frame
+figure,
+%for i=100:no_frames
+for i = 180:no_frames
     frame = read(vid,i);
     
-    %% Plaats hier je verwerking per frame %%
+    %%Plaats hier je verwerking per frame %%
+    
+    %%Voorgrond
+    voorgrondFrame = read(vid, i);
+    %figure('name', 'frame'), imshow(voorgrondFrame)
+    xVT = rgb2gray(voorgrondFrame);
+    xV = im2double(xVT);
+    
+    xV = imclose(xV, se_tekst);
+    
+    xV = medfilt2(xV, medf);
+    %%Verschil
+    xVerschil = xA - xV;
+ 
+    %figure('name', 'verschil'), imshow(xVerschil)
+    
+    %%section 3 Make uniform
+    %%Uniform maken
+    %xVerschil = histeq(xVerschil, 100000);
+    %figure('name', 'histeq'), imshow(xVerschil)
+    
+    %% 
+
+    fBlack = xVerschil> 0.21;
+    %fBlack = bwmorph(fBlack, 'clean');
+    
+    %figure('name', 'to black'), imshow(fBlack)
+    
+    %Opening by reconstruction
+    fe=imerode(fBlack,ones(80,30));
+    fBlack1=imreconstruct(fe,fBlack);
+    
+    %figure('name', 'reconstruct'), imshow(fBlack1)
+    %imshow(fBlack1)
+    %%figure, imshow(xVerschil)
+    %%figure, imhist(xVerschil)
+    %%Opening/closing
+    se = strel('disk', 10);
+    %se_tekst = strel('rectangle', [25, 15]);
+    %figure('name', 'opening'), imshow(fBlack)
+    fo = imopen(fBlack1,se);
+    imshow(fo)
     
     %strategie:
     %Detecteer per frame of er een object verschijnt
